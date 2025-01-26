3cb50b7656a1e7ece8a086e516eea6fb3b7a13cd
vivek mishra <vivek45mishra45@gmail.com>
2025-01-26 16:30:39 +0530

    java file add

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static int multiply(int n1, int n2, int b) {
+        int rv = 0;
+        int p = 0;
+        System.err.println(rv);
+        
+ return rv;
+    }
+
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+
+        int n1 = Sc.nextInt();
+        int n2 = Sc.nextInt();
+        int b = Sc.nextInt();
+
+        int d = multiply(n1, n2, b);
+
+        System.out.println(d);
+
+    }
+}
